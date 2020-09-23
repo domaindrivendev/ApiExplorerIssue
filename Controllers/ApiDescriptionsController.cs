0cd5c60b888553c82a61e95b5a9bc63f90018015
rmorris <richard.morris@gettyimages.com>
2020-09-23 19:39:42 +0100

    Initial commit

---
+++ Controllers/ApiDescriptionsController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace ApiExplorerIssue.Controllers
+{
+    [Route("[controller]")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ApiDescriptionsController : Controller
+    {
+        private readonly IApiDescriptionGroupCollectionProvider _apiDescriptionGroupCollectionProvider;
+
+        public ApiDescriptionsController(IApiDescriptionGroupCollectionProvider apiDescriptionGroupCollectionProvider)
+        {
+            _apiDescriptionGroupCollectionProvider = apiDescriptionGroupCollectionProvider;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var apiDescriptions = _apiDescriptionGroupCollectionProvider.ApiDescriptionGroups.Items
+                .SelectMany(group => group.Items)
+                .Select(apiDescription =>
+                    new
+                    {
+                        relativePath = apiDescription.RelativePath,
+                        httpMethod = apiDescription.HttpMethod,
+                        parameters = apiDescription.ParameterDescriptions
+                            .Select(parameterDescription =>
+                                new
+                                {
+                                    name = parameterDescription.Name,
+                                    source = parameterDescription.Source
+                                })
+                    });
+
+            return new ObjectResult(apiDescriptions);
+        }
+    }
+}
---
+++ Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiExplorerIssue.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [Route("foo")]
+        [Route("bar/{id}")]
+        public IActionResult Get(int id = 0)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
