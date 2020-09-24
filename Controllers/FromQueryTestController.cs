15130ef6eb5aa44f3f23ddf91574edf1f93afff5
mkopytin <mkopytin@custis.ru>
2020-09-24 15:47:51 +0300

    new issuie

---
+++ Controllers/FromQueryTestController.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiExplorerIssue.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet("get-foo")]
+        public DataQueryOptions? Foo([FromQuery] DataQueryOptions? queryOptions)
+        {
+            return queryOptions;
+        }
+
+        public class DataQueryOptions
+        {
+            /// <summary> Paging settings </summary>
+            [FromQuery(Name = "paging")]
+            public DataQueryPaging? Paging { get; set; }
+        }
+
+        public sealed class DataQueryPaging
+        {
+            [FromQuery(Name = "top")]
+            [Required]
+            public int Top { get; set; }
+
+            [FromQuery(Name = "skip")]
+            [Required]
+            public int Skip { get; set; }
+
+            [FromQuery(Name = "count")]
+            public bool? Count { get; set; }
+        }
+    }
+}
